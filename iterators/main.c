b5c26426119c99328160b676416da814344d31c0
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-09-22 13:28:43 +0200

    Added iterators

---
+++ iterators/main.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct {
+  size_t i;
+  size_t end;
+  uint32_t *data;
+  uint32_t *element;
+} Iterator;
+
+Iterator iter(uint32_t *array, size_t size) {
+  return (Iterator){
+      .i = 0,
+      .data = array,
+      .end = size,
+      .element = array,
+  };
+}
+
+bool iter_end(Iterator *iterator) { return iterator->i < iterator->end; }
+
+uint32_t *next(Iterator *iterator) {
+  assert(iterator->i < iterator->end);
+  iterator->element = &iterator->data[++iterator->i];
+  return iterator->element;
+}
+
+int main(void) {
+  uint32_t array[4] = {1, 2, 3, 4};
+  for (Iterator it = iter(array, 4); iter_end(&it); next(&it)) {
+    printf("%d\n", *it.element);
+  }
+}
