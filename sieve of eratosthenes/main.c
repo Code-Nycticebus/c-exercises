ae10a4a61a0dea2680ccec51461b04bab2c6a67a
Code-Nycticebus <loris.kriyonas@gmail.com>
2023-09-22 07:42:23 +0200

    Added sieve of some guy

---
+++ sieve of eratosthenes/main.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int32_t input(const char *msg) {
+#define MAX_INPUT 11
+  if (msg) {
+    printf("%s", msg);
+  }
+  char user_input_str[MAX_INPUT];
+  fgets(user_input_str, MAX_INPUT, stdin);
+  return strtol(user_input_str, NULL, 10);
+#undef MAX_INPUT
+}
+
+int main(void) {
+  uint32_t N = input("Input Limit: ");
+  bool *array = calloc(N, sizeof(bool));
+
+  for (size_t i = 2; i < N; ++i) {
+    if (array[i] == false) {
+      uint32_t multiples = i + i;
+      while (multiples < N) {
+        array[multiples] = true;
+        multiples += i;
+      }
+    }
+  }
+
+  for (size_t i = 2; i < N; ++i) {
+    if (array[i] == false) {
+      printf("%d\n", i);
+    }
+  }
+
+  free(array);
+}
