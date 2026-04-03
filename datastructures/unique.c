e696a94cc1f64d621b636293311243f9ea9d8969
Code-Nycticebus <loris.kriyonas@gmail.com>
2026-04-03 23:20:57 +0200

    further improved unique pointers

--- datastructures/unique.c
+++ datastructures/unique.c
@@ -10,19 +10,31 @@
          ? (printf("%s:%d: '%s' but already moved!\n", __FILE__, __LINE__, action), abort())       \
          : (void)0)
 
-#define Unique(T) struct { bool moved; T* data; }
 
-#define unique_alloc(T)  {.data = calloc(1, sizeof(T)), .moved = false}
+#define Unique(T)                                                                                  \
+    struct {                                                                                       \
+        bool moved;                                                                                \
+        T *data;                                                                                   \
+        void (*deleter)(void*);                                                                    \
+    }
+
+// unique_make(ptr, optional: free)
+#define unique_make(...) {false, __VA_ARGS__}
 #define unique_free(ptr)                                                                           \
     do {                                                                                           \
-        _unique_check(ptr, "unique_free");                                                         \
-        free((ptr).data);                                                                          \
+        _unique_check(ptr, "free");                                                                \
+        ((ptr).deleter) ? (ptr).deleter((ptr).data) : free((ptr).data);                            \
         (ptr).data = NULL;                                                                         \
         (ptr).moved = true;                                                                        \
     } while (0)
 
-#define unique_get(ptr) (_unique_check(ptr, "unique_get"), (ptr).data)
-#define unique_move(ptr) {.data = (_unique_check(ptr, "unique_move"), (ptr).moved = true, (ptr).data)}
+#define unique_get(ptr) (_unique_check(ptr, "get"), (ptr).data)
+#define unique_move(ptr)                                                                           \
+    {                                                                                              \
+        .moved = (_unique_check(ptr, "move"), false),                                              \
+        .data = ((ptr).moved = true, (ptr).data),                                                  \
+        .deleter = (ptr).deleter,                                                                  \
+    }
 
 // USER CODE
 
@@ -30,7 +42,7 @@ typedef Unique(int) UniqueInt;
 
 // a function that creates an object
 UniqueInt make_int(int value) {
-    UniqueInt i = unique_alloc(int);
+    UniqueInt i = unique_make(malloc(sizeof(int)), .deleter=NULL);
     *unique_get(i) = value;
     return (UniqueInt)unique_move(i);
 }
@@ -49,7 +61,7 @@ int main(void) {
     take_int((UniqueInt)unique_move(my_uptr));
 
     // all these errors get catched!
-    // *unique_get(my_uptr) = 420;                // use after free
+    // *unique_get(my_uptr) = 69;                 // use after free
     // take_int((UniqueInt)unique_move(my_uptr)); // multiple moves
     // unique_free(my_uptr);                      // double free
 }
